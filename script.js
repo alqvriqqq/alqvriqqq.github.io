1c335d0ced529c0196145c348b7d19edc041e4df
alqvriqqq <omerlamaj@icloud.com>
2024-06-11 17:54:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
 function displayFileContent() {
-    var fileInput = document.getElementById('fileInput');
-    var file = fileInput.files[0];
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
     if (file) {
-        var reader = new FileReader();
+        const reader = new FileReader();
         reader.readAsText(file);
         reader.onload = function() {
-            var content = reader.result;
+            const content = reader.result;
             displayRawContent(content);
         };
     }
-}
-
-function upload() {
-    var textInput = document.getElementById('textInput');
-    var content = textInput.value;
-    if (!content && document.getElementById('rawText').textContent) {
-        content = document.getElementById('rawText').textContent;
-    }
-
-    if (!content) {
-        document.getElementById('message').textContent = 'No content to upload';
-        return;
-    }
-
-    createDownloadLink(content);
-    document.getElementById('message').textContent = 'Content ready for download';
 }
 
 function displayRawContent(content) {
@@ -34,11 +18,31 @@ function displayRawContent(content) {
     document.getElementById('downloadLink').style.display = 'none';
 }
 
-function createDownloadLink(content) {
-    var blob = new Blob([content], { type: 'text/plain' });
-    var url = URL.createObjectURL(blob);
-    var downloadLink = document.getElementById('download');
+function prepareDownload() {
+    const textInput = document.getElementById('textInput');
+    const fileNameInput = document.getElementById('fileName');
+    let content = textInput.value;
+    let fileName = fileNameInput.value || 'fileguest';
+
+    if (!content && document.getElementById('rawText').textContent) {
+        content = document.getElementById('rawText').textContent;
+    }
+
+    if (!content) {
+        document.getElementById('message').textContent = 'No content to prepare for download';
+        return;
+    }
+
+    createDownloadLink(fileName, content);
+    document.getElementById('message').textContent = 'Content ready for download';
+}
+
+function createDownloadLink(fileName, content) {
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
+    const downloadLink = document.getElementById('download');
     downloadLink.href = url;
+    downloadLink.download = `${fileName}.txt`;
     downloadLink.style.display = 'block';
     document.getElementById('downloadLink').style.display = 'block';
 }
